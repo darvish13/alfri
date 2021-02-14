67d3a237b247de173a8349a19bf5c939d0bd997a
Alireza Lavasani <a.lavasani@farabeen.com>
2021-02-14 09:29:23 +0330

    changed homepage component

--- src/App.js
+++ src/App.js
@@ -9,8 +9,9 @@ const App = () => (
   <BrowserRouter>
     <Suspense fallback={() => <LinearProgress />}>
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route path='/ablue' component={ABlue} />
+        {/* <Route exact path='/' component={Home} /> */}
+        <Route exact path='/' component={ABlue} />
+        {/* <Route path='/ablue' component={ABlue} /> */}
       </Switch>
     </Suspense>
   </BrowserRouter>
